41a9e33561956dae7d9fbdeb2d6a5f423d43c748
Jonne Hirvi <jonnehir@gmail.com>
2019-11-23 18:37:38 +0200

    Updates

---
+++ precache-manifest.7eac70ad0d9a120b33b45d4bd759a8fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351193720d93368f5ab117b88298b1fe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2037a812e7696170fae4",
+    "url": "/monsters-rolodex/static/css/main.2b3aa527.chunk.css"
+  },
+  {
+    "revision": "d8097c85df4a798a5802",
+    "url": "/monsters-rolodex/static/js/2.278bbdac.chunk.js"
+  },
+  {
+    "revision": "2037a812e7696170fae4",
+    "url": "/monsters-rolodex/static/js/main.ab34446a.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
